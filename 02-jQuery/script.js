490902022a90ece78692f787ec048c2e73c31113
Sebastien David <sebastien.david.1983@gmail.com>
2012-11-16 16:31:46 +0100

    fix for...in loop

--- 02-jQuery/script.js
+++ 02-jQuery/script.js
@@ -1,31 +1,54 @@
 function callTwitter() {
-	var query = $('#myQuery').val();
+	var query = document.getElementById('myQuery').value;
+	var that = this;
 
-	$.getJSON('/search?q='+query, function(data){
-		var results = data.results;
-		var $responseDiv =  $('#response');
-		$responseDiv.empty();
-		$.each(results, function(key, val){
-			$responseDiv.append('<li><img src="'+val.profile_image_url+'" /><b>'+val.from_user+':</b><span>'+val.text+'</span></li>');
-		});
-		setListeners();
-	});
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', '/search?q='+query, true);
+	xhr.onload = function(e) {
+		var results = JSON.parse(xhr.responseText).results;
+		var responseDiv =  document.getElementById('response');
+		while (responseDiv.firstChild){
+			responseDiv.removeChild(responseDiv.firstChild);
+		}
+		
+		for (var val, i = 0, length=results.length; i < length; i++){
+			var li = document.createElement("li");
+			var img = document.createElement("img");
+			val = results[i];
+			img.setAttribute("src",val.profile_image_url);
+			var b = document.createElement("b");
+			b.textContent=val.from_user+":";
+			var span = document.createElement("span");
+			span.textContent=val.text;
+			li.appendChild(img);
+			li.appendChild(b);
+			li.appendChild(span);
+			setListener(li);
+			responseDiv.appendChild(li);
+		}
+	}
+	xhr.send(); 
 }
 
-function setListeners(){
-	$('li img').css('opacity',0.4);
-	$('li').hover(function(e){
-		$(this).find('img').css('opacity',1);
-	},function(e){
-		$(this).find('img').css('opacity',0.4);
-	});
+function setListener(li){
+	var imgs = li.getElementsByTagName("img");
+	var img = imgs[0];
+	img.setAttribute('style','opacity:0.4');
+	li.onmouseover=function(event){
+		event.currentTarget.getElementsByTagName("img")[0].setAttribute('style','opacity:1');
+	};
+	li.onmouseout=function(event){
+		event.currentTarget.getElementsByTagName("img")[0].setAttribute('style','opacity:0.4');
+	};
 }
 
-$(document).ready(function(){
-	$('#myQuery').on('keyup', function(e){
-		if (e.keyCode == 13){
+function init(){
+	document.getElementById('myQuery').addEventListener('keyup', function(e){
+		if (e.keyCode==13){
 			callTwitter();
 		}
-	});
-	$('#myBtn').on('click', callTwitter);
-});
+	})
+	document.getElementById('myBtn').addEventListener('click', callTwitter);
+}
+
+document.addEventListener("DOMContentLoaded", init, false);
